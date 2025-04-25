ebbaa6531d134a52c7756acfa6b0057cd51aa1fa
Ane-Marrie <iamsabau@yahoo.co.uk>
2025-04-25 19:21:43 +0100

    Add options for rainbow colored cells on hover, grid-like canvas by adding cell borders and "reset all" button.

--- script.js
+++ script.js
@@ -1,51 +1,96 @@
 const containerDiv = document.querySelector(".container");
+const controls = document.querySelector(".controls");
+const title = document.createElement("h2");
+title.textContent = "Etch-a-Sketch";
+controls.appendChild(title);
 
+const buttonCustomise = document.createElement("button");
+buttonCustomise.textContent = "Make your own !"
+controls.appendChild(buttonCustomise);
 
-//add function to create 16x16 grid instead of manualy copy and paste divs in html
-function createDivs(num) {
+const buttonRainbow = document.createElement("button");
+buttonRainbow.setAttribute("id", "rainbow_button");
+buttonRainbow.textContent = "Rainbow";
+controls.appendChild(buttonRainbow);
 
-    for(let i=0; i<num; i++) {
-        const singleDiv = document.createElement("div");
-        singleDiv.classList.add("single");
-        containerDiv.appendChild(singleDiv);
-    }
-    addHoverEffect();
-}
+const buttonGridMode = document.createElement("button");
+buttonGridMode.textContent = "Grid Mode";
+controls.appendChild(buttonGridMode);
 
-createDivs(256);
+const buttonReset = document.createElement("button");
+buttonReset.textContent = "Reset";
+controls.appendChild(buttonReset);
 
-//add event listeners for "hover" effect
-function addHoverEffect() {
-    const allDivs = document.querySelectorAll("div");
-    allDivs.forEach(elem => {
-        elem.addEventListener("mouseover", () => {
-            elem.style.backgroundColor = "black";
-        })
-        elem.addEventListener("mouseout", () => {
-            elem.style.backgroundColor = "white";
+//function to create grid
+function createGrid(num) {
+    containerDiv.textContent = "";
+    const total = num ** 2;
+    const size = 500 / num;
+
+        for(let i=0; i<total; i++) {
+            const cell = document.createElement("div");
+            cell.style.width = `${size}px`;
+            cell.style.height = `${size}px`;
+            cell.classList.add("grid");
+            containerDiv.appendChild(cell);    
+        }
+
+        //event listener for "hover" effect
+        document.querySelectorAll(".grid").forEach(cell => {
+            cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "black";
+            const currentOpacity = cell.style.opacity;
+                if(currentOpacity) {
+                    cell.style.opacity = Number(currentOpacity) + 0.1;
+                }
+                else {
+                    cell.style.opacity = 0.2;
+                }
         })
     })
-}
+} 
 
-//add option for user to create own grid
-const body = document.querySelector("body");
-const promptButton = document.createElement("button");
-promptButton.textContent = "Make your own !"
-body.appendChild(promptButton);
-body.insertBefore(promptButton, containerDiv)
+createGrid(16);
 
-promptButton.addEventListener("click", () => {
-    const input = prompt("Please enter the number of squares per side for your new grid. This should be a number between 1-100");
-    const square = Math.pow(input, 2);
-
-    if(input >= 1 && input <= 100) {
-        containerDiv.textContent = "";
-        createDivs(square);
-        addHoverEffect();
+//option for user to create own grid
+buttonCustomise.addEventListener("click", () => {
+    containerDiv.textContent = "";
+    const input = prompt("Please enter the number of squares per side for your new grid. This should be a number between 1-100.");
+    if(input > 0 && input <= 100) {
+        createGrid(input);
     }
     else {
-        prompt("Please enter a number between 1-100");
+        alert("Wrong input. Please enter a number between 1-100.");
     }
 
 })
 
+//option for rainbow colors on hover
+buttonRainbow.addEventListener('click', ()=>{
+    containerDiv.querySelectorAll("div").forEach(div => {
+        div.addEventListener('mouseover', () => {
+            div.classList.add("rainbow");
+            const color = Math.floor(Math.random()*16777215).toString(16);
+            div.style.backgroundColor=`#${color}`;
+            const currentOpacity = div.style.opacity;
+                if(currentOpacity) {
+                    div.style.opacity = Number(currentOpacity) + 0.1;
+                }
+                else {
+                    div.style.opacity = 0.3;
+                }
+            })
+        })
+    })
+
+//option for grid-like canvas
+buttonGridMode.addEventListener('click', ()=>{
+    containerDiv.querySelectorAll("div").forEach(div => {
+        div.style.border = "1px solid black";
+    })
+})
+
+//reset all
+buttonReset.addEventListener('click', ()=>{
+    createGrid(16);
+})
